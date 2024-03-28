dae846b5367fd8604e6db5411152358ddd883472
Nazmul Islam <litocse@gmail.com>
2024-03-29 04:00:44 +0600

    Added Controller

---
+++ Services/Catalog/Catalog.API/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
